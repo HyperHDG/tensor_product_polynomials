a88763742c05d8644dd14fb346328a75b377c27d
andreas <andreas.rupp@fau.de>
2021-01-09 20:37:30 +0100

    Make initial things work.

--- include/tpp/compile_time_tricks.hxx
+++ include/tpp/compile_time_tricks.hxx
@@ -1,52 +1,9 @@
 #pragma once  // Ensure that file is included only once in a single compilation.
 
 #include <limits>
-#include <type_traits>
-#include <utility>
 
-//namespace TPP
-//{
-
-/*!*************************************************************************************************
- * \brief   Unused parametes will neither result in g++, nor in doxygen warnings if wrapped by this.
- **************************************************************************************************/
-#define UNUSED(x) /* nothing */
-
-/*!*************************************************************************************************
- * \brief   Check if some class implements some function with some signature.
- *
- * This macro receives the name of the function that is checked to be implemented with a given
- * signature (not handled to the macro itself) and the name of a struct (output) that can be used
- * to check whether function func is implemented.
- *
- * Having invoked the macro, we are able to use the template struct \c name to check whether
- * function \c fun is a (static or non-static) member function of an element of class \c C, where
- * \c Ret(Args) is the supposed signature.
- *
- * \param   func    The name of the function that is checked to be implemented.
- * \param   name    The resulting struct whose value is true if the function is implemented.
- **************************************************************************************************/
-#define HAS_MEMBER_FUNCTION(func, name)                                                            \
-  template <typename, typename T>                                                                  \
-  struct name                                                                                      \
-  {                                                                                                \
-    static_assert(std::integral_constant<T, false>::value,                                         \
-                  "Second template parameter must be function signature.");                        \
-  };                                                                                               \
-  template <typename C, typename Ret, typename... Args>                                            \
-  struct name<C, Ret(Args...)>                                                                     \
-  {                                                                                                \
-   private:                                                                                        \
-    template <typename T>                                                                          \
-    static constexpr auto check(T*) ->                                                             \
-      typename std::is_same<decltype(std::declval<T>().func(std::declval<Args>()...)), Ret>::type; \
-    template <typename>                                                                            \
-    static constexpr std::false_type check(...);                                                   \
-    typedef decltype(check<C>(0)) type;                                                            \
-                                                                                                   \
-   public:                                                                                         \
-    static constexpr bool value = type::value;                                                     \
-  }
+namespace TPP
+{
 
 /*!*************************************************************************************************
  * \brief   Calculate the non-negative square-root of a non-negative number at compile time.
@@ -75,4 +32,4 @@ static constexpr float_t heron_root(const float_t square)
   return 0.5 * (lower_root + upper_root);
 }
 
-//} // end of namespace TPP
+} // end of namespace TPP
